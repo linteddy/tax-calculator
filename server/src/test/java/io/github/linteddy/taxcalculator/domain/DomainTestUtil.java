89ba382601cb5202f27384355a0ce5756945c22e
Linda Ngwenya <linda.ngwenya@absa.africa>
2021-03-13 00:13:08 +0200

    refactor

--- server/src/test/java/io/github/linteddy/taxcalculator/domain/DomainTestUtil.java
+++ server/src/test/java/io/github/linteddy/taxcalculator/domain/DomainTestUtil.java
@@ -1,6 +1,8 @@
 package io.github.linteddy.taxcalculator.domain;
 
 import java.math.BigDecimal;
+import java.util.HashSet;
+import java.util.Set;
 
 public class DomainTestUtil {
 
@@ -10,5 +12,13 @@ public class DomainTestUtil {
         medicalAidTaxCredits.setFirstDependant(BigDecimal.valueOf(319));
         medicalAidTaxCredits.setAdditionalDependant(BigDecimal.valueOf(215));
         return medicalAidTaxCredits;
+    }
+
+    static Set<TaxBracket> createTaxBrackets(){
+        Set<TaxBracket> taxBrackets = new HashSet<>();
+        taxBrackets.add(new TaxBracket(BigDecimal.ZERO, BigDecimal.valueOf(205900), 18, null));
+        taxBrackets.add(new TaxBracket(BigDecimal.valueOf(205901), BigDecimal.valueOf(321600), 26, BigDecimal.valueOf(37062)));
+        taxBrackets.add(new TaxBracket(BigDecimal.valueOf(1577301), null, 45, BigDecimal.valueOf(559464)));
+        return taxBrackets;
     }
 }
--- server/src/test/java/io/github/linteddy/taxcalculator/domain/TaxBracketTest.java
+++ server/src/test/java/io/github/linteddy/taxcalculator/domain/TaxBracketTest.java
@@ -1,42 +1,25 @@
 package io.github.linteddy.taxcalculator.domain;
 
 
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
-import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestMethodOrder;
 
 import java.math.BigDecimal;
-import java.util.HashSet;
 import java.util.Optional;
 import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@TestMethodOrder(OrderAnnotation.class)
 class TaxBracketTest {
 
-    private static Set<TaxBracket> taxBrackets;
-
-    @BeforeAll
-    static void setUp() {
-        taxBrackets = new HashSet<>();
-        taxBrackets.add(new TaxBracket(BigDecimal.ZERO, BigDecimal.valueOf(205900), 18, null));
-        taxBrackets.add(new TaxBracket(BigDecimal.valueOf(205901), BigDecimal.valueOf(321600), 26, BigDecimal.valueOf(37062)));
-        taxBrackets.add(new TaxBracket(BigDecimal.valueOf(1577301), null, 45, BigDecimal.valueOf(559464)));
-    }
+    private final Set<TaxBracket> taxBrackets = DomainTestUtil.createTaxBrackets();
 
     @Test()
     @DisplayName("TaxBracket meets the minimum requirements for HashSet")
-    @Order(1)
     void testEquality() {
         assertThat(taxBrackets).hasSize(3);
         taxBrackets.add(new TaxBracket(BigDecimal.valueOf(1577301), null, 45, BigDecimal.valueOf(559464)));
         assertThat(taxBrackets).hasSize(3);
-        taxBrackets.add(new TaxBracket(BigDecimal.valueOf(321601), BigDecimal.valueOf(445100), 31, BigDecimal.valueOf(67144)));
-        assertThat(taxBrackets).hasSize(4);
     }
 
     @Test
