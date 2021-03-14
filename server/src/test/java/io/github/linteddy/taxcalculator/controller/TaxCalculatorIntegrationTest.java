832483c98e65626b93ddb2c96f9d4758efff2a44
Linda Ngwenya <linda.ngwenya@absa.africa>
2021-03-14 20:04:35 +0200

    add 2020 tax tables

--- server/src/test/java/io/github/linteddy/taxcalculator/controller/TaxCalculatorIntegrationTest.java
+++ server/src/test/java/io/github/linteddy/taxcalculator/controller/TaxCalculatorIntegrationTest.java
@@ -19,8 +19,8 @@ class TaxCalculatorIntegrationTest {
     private WebTestClient webClient;
 
     @Test
-    @DisplayName("Should calculate monthly income tax")
-    void monthlyIncomeTax() throws Exception {
+    @DisplayName("Should calculate 2021 monthly income tax")
+    void monthlyIncomeTaxFor2021() throws Exception {
         webClient.get()
                 .uri(
                         uriBuilder -> uriBuilder
@@ -44,8 +44,8 @@ class TaxCalculatorIntegrationTest {
     }
 
     @Test
-    @DisplayName("Should calculate annual income tax")
-    void annualIncomeTax() throws Exception {
+    @DisplayName("Should calculate 2021 annual income tax")
+    void annualIncomeTaxFor2021() throws Exception {
         webClient.get()
                 .uri(
                         uriBuilder -> uriBuilder
@@ -65,6 +65,31 @@ class TaxCalculatorIntegrationTest {
                     assertThat(incomeTaxResult.getTaxCredits()).isEqualTo("10236");
                     assertThat(incomeTaxResult.getPayAsYouEarnAfterTaxCredit()).isEqualTo("45655.00");
                     assertThat(incomeTaxResult.getNetCash()).isEqualTo("314345.00");
+                });
+    }
+
+    @Test
+    @DisplayName("Should calculate 2020 annual income tax")
+    void annualIncomeTaxFor2020() throws Exception {
+        webClient.get()
+                .uri(
+                        uriBuilder -> uriBuilder
+                                .path("/api/tax/income-tax")
+                                .queryParam("year", 2020)
+                                .queryParam("age", 70)
+                                .queryParam("income", BigDecimal.valueOf(360_000))
+                                .queryParam("period", Period.ANNUALLY)
+                                .queryParam("medicalAidMembers", 3)
+                                .build())
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange().expectStatus().isOk()
+                .expectHeader().contentType(MediaType.APPLICATION_JSON)
+                .expectBody(IncomeTaxResult.class)
+                .value(incomeTaxResult -> {
+                    assertThat(incomeTaxResult.getPayAsYouEarnBeforeTaxCredit()).isEqualTo("58625.50");
+                    assertThat(incomeTaxResult.getTaxCredits()).isEqualTo("9948");
+                    assertThat(incomeTaxResult.getPayAsYouEarnAfterTaxCredit()).isEqualTo("48677.50");
+                    assertThat(incomeTaxResult.getNetCash()).isEqualTo("311322.50");
                 });
     }
 
